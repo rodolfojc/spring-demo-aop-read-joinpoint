c4a5125620b66696048c725e403fd7388cd7bd0b
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-05 23:08:44 +0000

    Display Method Signature
    Demo

--- src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -1,7 +1,9 @@
 package com.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +14,16 @@ public class MyDemoLogginAspect {
 	
 	//@BEFORE
 	@Before("com.aopdemo.aspect.AopExpressions.forDaoPackageNoSettersOrGetters()")
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 		
 		System.out.println("Executing @Before advice on addAccount()");
+		
+		//DISPLAY METHOD SIGNAURE
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: "+methodSig);
+		
+		//DISPLAY METHOD ARGUMENTS
+		
 		
 	}
 	
